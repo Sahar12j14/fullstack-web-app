7c20ad378668518e3ac6b453f27af9e1ad2e72c2
Sahar12j14 <71217328+Sahar12j14@users.noreply.github.com>
2021-01-19 15:05:27 +0300

    update

--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 const JSONfileName = path.resolve(__dirname, 'products.json');
+const express = require('express');
+const app = express();
+
+app.get('/products', (req, res) => {
+  res.send(getAllProducts(JSONfileName));
+});
+
+app.get("/product/:id", (req, res) => {
+  res.send(getElementId(JSONfileName, Number(req.params.id)));
+});
+app.listen(80);
+
 //получение всех продуктов
 function getAllProducts(fileJSON) {
   return JSON.parse(
@@ -49,62 +61,61 @@ function addElement(fileJSON, newProductName, newProductPrice, newProductAmount)
           console.error(err);
         }
       });
-//обновление существующего продукта по указанному id
-function upProduct(fileJSON, newProductName, newProductPrice, newProductAmount, newId) {
-    let newProduct = {
-      id: newId,
-      product_name: newProductName,
-      product_price: newProductPrice,
-      product_amount: newProductAmount,
-    };
-    const product = JSON.parse(
-      fs.readFileSync(fileJSON, 'utf8', (err, data) => {
-        if (err) {
-          console.error(err);
-        }
-      })
-    );
-    product[
-      product.findIndex(function searchById(element, index, array) {
-        if (element.id === newId) {
-          return true;
-        }
-      })
-    ] = newProduct;
+    }
+//удаление продукта
+function deleteElement(fileJSON, id) {
+  const product = JSON.parse(
+    fs.readFileSync(fileJSON, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      }
+    })
+  );
+  let deleteIndex = product.findIndex((element, index, array) => {
+    if (element.id === id) {
+      return true;
+    }
+  });
+  if (deleteIndex !== (-1)){
+    product.splice(deleteIndex, 1);
     fs.writeFileSync(fileJSON, JSON.stringify(product), (err) => {
       if (err) {
         console.error(err);
       }
     });
-  }
-//удаление продукта
-function deleteProduct(fileJSON, id) {
-    const product = JSON.parse(
-      fs.readFileSync(fileJSON, 'utf8', (err, data) => {
-        if (err) {
-          console.error(err);
-        }
-      })
-    );
-    let deleteIndex = product.findIndex((element, index, array) => {
-      if (element.id === id) {
-        return true;
-      }
-    });
-    if (deleteIndex !== (-1)){
-      product.splice(deleteIndex, 1);
-      fs.writeFileSync(fileJSON, JSON.stringify(product), (err) => {
-        if (err) {
-          console.error(err);
-        }
-      });
-      return true;
-    }
+    return true;
   }
 }
-
-console.log(getAllProducts(JSONfileName));
-console.log(getElementId(JSONfileName, 2));
-console.log(addElement(JSONfileName, 'BMW', 4500000, 10));
-console.log(updateProduct(JSONfileName,"Toyota",3500000,25,1));
-console.log(deleteProduct(JSONfileName, 2));
+//обновление существующего продукта по указанному id
+function update(fileJSON, newProductName, newProductPrice, newProductAmount, newId){
+  let newProduct = {
+    id: newId,
+    product_name: newProductName,
+    product_price: newProductPrice,
+    product_amount: newProductAmount,
+  };
+  const product = JSON.parse(
+    fs.readFileSync(fileJSON, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      }
+    })
+  );
+  product[
+    product.findIndex(function searchById(element, index, array) {
+      if (element.id === newId) {
+        return true;
+      }
+    })
+  ] = newProduct;
+  fs.writeFileSync(fileJSON, JSON.stringify(product), (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+//console.log(getAllProducts(JSONfileName));
+//console.log(getElementId(JSONfileName, 2));
+//addElement(JSONfileName, 'BMW', 4500000, 10);
+//deleteElement(JSONfileName, 5);
+//console.log(update(JSONfileName,"Toyota",3500000,25,4));
